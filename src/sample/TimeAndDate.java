8fde783da9f3fac405d91795b3a1730b5f7dedc2
Sm1the <apxahreji123@gmail.com>
2020-11-26 16:45:39 +0700

    Create abstract class - TimeAndDate, class - DownLoader

---
+++ src/sample/TimeAndDate.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.util.Date;
+
+public abstract class TimeAndDate {
+
+    private Date date = new Date();
+
+    protected Date getTimeAndDate() {
+        return date;
+    }
+}
